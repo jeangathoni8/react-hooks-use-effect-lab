6f866ddb16dc4cce9708d382344558ae8853c232
jeangathoni8 <jeangathoni39@gmail.com>
2024-04-29 15:19:21 +0300

    use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// Question component
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect (() => {
+    //if the timer reaches 0, reset it and call onAnswered(false)
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; //Prevent setting up a new timeout if time ran out
+    }
+
+    //Set up a timeout to decrease the timer every second
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
+    //Cleanup function to clear the timeout if the component unmounts or updates
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]); //Add onAnswered to the dependency array
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
